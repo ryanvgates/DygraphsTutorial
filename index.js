5feae1656fd9418e98321c8a6bd71a6bd183dcf8
Ryan Gates <ryanvgates@gmail.com>
2015-04-01 00:19:01 -0400

    Removed scientific notation

--- index.js
+++ index.js
@@ -6,6 +6,22 @@
     // CSV or path to a CSV file.
     "Fort Lauderdale Data Snapshot-Reoriented.csv",
 	{
-		title: 'Fort Lauderdale Budget Data'
+		title: 'Fort Lauderdale Budget Data',
+        width: 800,
+        height: 480,
+        axisLabelWidth: 100,
+        maxNumberWidth: 20,
+        axis: {
+            y: {
+                axisLabelFormatter: function(x) {
+                    return '' + x;
+                },
+                axisValueFormatter: function(x) {
+                    return '' + x;
+                },
+            }
+        }
+        
+
 	}
   );
