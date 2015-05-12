d3886d85cc789f17f4bf5782e87744b337246d7d
Ryan Gates <ryanvgates@gmail.com>
2015-05-11 22:51:40 -0400

    Added Step 1 - Adding a title

---
+++ Step 1 - Adding a Title/index.js
@@ -0,0 +1,11 @@
+  g = new Dygraph(
+
+    // containing div
+    document.getElementById("graphdiv"),
+
+    // CSV or path to a CSV file.
+    "Fort Lauderdale Data Snapshot-Reoriented.csv",
+    {
+    	title: 'Fort Lauderdale Budget Data'
+    }
+  );
