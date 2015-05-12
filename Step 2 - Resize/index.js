5aca1d5a16904d2faa527e736243209a1a7b57d1
Ryan Gates <ryanvgates@gmail.com>
2015-05-11 23:15:35 -0400

    Created Step 2 - Resize

---
+++ Step 2 - Resize/index.js
@@ -0,0 +1,13 @@
+  g = new Dygraph(
+
+    // containing div
+    document.getElementById("graphdiv"),
+
+    // CSV or path to a CSV file.
+    "Fort Lauderdale Data Snapshot-Reoriented.csv",
+    {
+    	title: 'Fort Lauderdale Budget Data',
+        width: 800,
+        height: 480,
+    }
+  );
