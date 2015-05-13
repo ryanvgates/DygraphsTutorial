b9b1642e6947b1bc7cc735f701f3234af52673aa
Ryan Gates <ryanvgates@gmail.com>
2015-05-13 00:34:51 -0400

    Added Step 4 - Set Axis Label Width

---
+++ Step 4 - Set Axis Label Width/index.js
@@ -0,0 +1,24 @@
+  g = new Dygraph(
+
+    // containing div
+    document.getElementById("graphdiv"),
+
+    // CSV or path to a CSV file.
+    "Fort Lauderdale Data Snapshot-Reoriented.csv",
+    {
+    	title: 'Fort Lauderdale Budget Data',
+        width: 800,
+        height: 480,
+        axisLabelWidth: 100,
+        axes: {
+            y: {
+                axisLabelFormatter: function(x) {
+                    return numeral(x).format('($ 0.00 a)');
+                },
+                valueFormatter: function(x) {
+                    return numeral(x).format('($ 0.00 a)');
+                },
+            }
+        },
+    }
+    );
